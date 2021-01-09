bae86995094664f935591b49fee8ba808acb7e1b
TakedaHiroki <hirokitakedaxxx@gmail.com>
2021-01-10 02:26:17 +0900

    upgrade create command

--- bot.py
+++ bot.py
@@ -23,6 +23,16 @@ guard = Guard()
 vote = Vote(survivors)
 
 
+def get_data(message):
+    command = message.content
+    data_table = {
+        '/members': message.guild.members,
+        '/roles': message.guild.roles,
+        '/text_channels': message.guild.text_channels,
+        '/voice_channels': message.guild.voice_channels,
+        '/category_channels': message.guild.categories,
+    }
+    return data_table.get(command, '無効なコマンドです')
 
 @bot.event
 async def on_ready():
@@ -54,7 +64,6 @@ async def on_message(message):
         else:
             await message.channel.send('もう一度やり直してください')
             
-
     elif message.content.startswith('投票→'):
         cmd, dst = message.content.split('→')
         if cmd == '投票' and dst in survivors:
@@ -63,28 +72,27 @@ async def on_message(message):
         else:
             await message.channel.send('もう一度やり直してください')
 
-    # new_channel = await create_private_text_channel(message)
-    # text = f'{new_channel.mention} を作成しました'
-    # await message.channel.send(text)
+    elif message.content.upper().startswith('\CREATE'):
+        for mention in message.mentions:
+            new_channel = await create_private_text_channel(message.guild, mention.id)
+            text = f'{new_channel.mention} を作成しました'
+            await message.channel.send(text)
+
+# @bot.event
+# async def create_text_channel(message, channel_name):
+#     category_id = message.channel.category_id
+#     category = message.guild.get_channel(category_id)
+#     new_channel = await category.create_text_channel(name=channel_name)
+#     return new_channel
 
 @bot.event
-async def create_text_channel(message, channel_name):
-    category_id = message.channel.category_id
-    category = message.guild.get_channel(category_id)
-    new_channel = await category.create_text_channel(name=channel_name)
-    return new_channel
-
-@bot.event
-async def create_private_text_channel(message):
-    category_id = message.channel.category_id
-    guild = message.guild
-    category = guild.get_channel(category_id)
+async def create_private_text_channel(guild, user_id):
     overwrites = {
         guild.default_role: discord.PermissionOverwrite(read_messages=False, send_messages=False),
         guild.me: discord.PermissionOverwrite(read_messages=True, send_messages=True),
-        guild.get_member(message.author.id): discord.PermissionOverwrite(read_messages=True, send_messages=True),
+        guild.get_member(user_id): discord.PermissionOverwrite(read_messages=True, send_messages=True),
     }
-    new_channel = await guild.create_text_channel(bot.get_user(message.author.id).display_name, overwrites=overwrites)
+    new_channel = await guild.create_text_channel(bot.get_user(user_id).display_name, overwrites=overwrites)
     return new_channel
 
 bot.run(TOKEN)
